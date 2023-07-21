b06ba130a093adfd274448bb7fa617945fb7c94d
Mateo Flores <dragonnegro1231@gmail.com>
2023-07-21 09:59:40 -0500

    Base del proyecto y estructura

--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import "../scss/SinglePage.scss";
+
 function SinglePage() {
   return (
-    <div>
-      <h1>SinglePage</h1>
-    </div>
+    <React.Fragment>
+      <div id="single-page--container">
+        <h1>Sistema de Analizis de Sentimientos</h1>
+        <div className="single-page-content">
+          <div className="twitter--container">
+            <h3>Usar Twitter</h3>
+            <input type="url" placeholder="Ingrese el link del twit a analizar" />
+            <button>Analizar</button>
+          </div>
+          <hr />
+          <div className="text--container">
+            <h3>Usar Texto</h3>
+            <textarea
+              placeholder="Ingrese el texto a analizar"
+              rows="10"
+              cols="50"
+            ></textarea>
+            <button>Analizar</button>
+          </div>
+        </div>
+      </div>
+      <div id="results--container">
+
+      </div>
+    </React.Fragment>
   );
 }
 
-export { SinglePage  };
+export { SinglePage };
